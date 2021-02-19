d28818d2fe9b9d864e43fe3b9c6fe7aad12055a3
Jessé Fernando <nandopox@gmail.com>
2021-02-19 14:57:35 -0300

    Repo initialyzed

---
+++ PersonalContactsList/Data/ContactsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalContactsList.Data
+{
+    class ContactsContext : DbContext
+    {
+        protected override void OnConfiguring ( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer( ConfigurationManager.ConnectionStrings["ContactsConn"].ConnectionString );
+        }
+    }
+}
